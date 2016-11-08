9fa2a244521d24fc09bd1c50af5245b15a4f24c7
philipptrenz <mail@philipptrenz.de>
2016-11-08 17:46:37 +0000

    added the start

---
+++ pyqt.py
@@ -0,0 +1,46 @@
+﻿#! /usr/bin/python3 
+import sys
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, 
+    QLabel, QApplication, QPushButton)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QObject
+
+
+class FreakingQtImageViewer(QWidget):
+    
+    def __init__(self, function):
+        super().__init__()
+        self.function = function
+        
+        self.initUI(function)
+        
+    def refresh(self):
+        self.function()
+        
+        pixmap = QPixmap("tmp.jpg")
+        pixmap = pixmap.scaledToWidth(800)
+        self.lbl.setPixmap(pixmap)
+        
+    def initUI(self, function):      
+
+        hbox = QHBoxLayout(self)
+        
+        self.lbl = QLabel(self)
+        self.refresh()
+        
+        btn = QPushButton(self)
+        btn.setText('Drück mich')
+        btn.clicked.connect(self.refresh)
+        
+        hbox.addWidget(self.lbl)
+        hbox.addWidget(btn)
+        self.setLayout(hbox)
+        
+        self.move(300, 200)
+        self.setWindowTitle('Freaking Qt Image Viewer')
+        self.show()
+
+
+        
+        
+
---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import io
+import os
+import picamera
+
+import time
+from time import sleep
+
+import numpy
+import picamera.array
+from PIL import Image
+
+from PyQt5.QtWidgets import QApplication
+from pyqt import FreakingQtImageViewer
+
+
+def capture():
+	with picamera.array.PiRGBArray(camera) as stream:
+		camera.capture(stream, format='rgb')
+		img = stream.array
+		print(img)
+		im = Image.fromarray(img)
+		im.save('./tmp.jpg')
+
+if __name__ == '__main__':
+        camera = picamera.PiCamera()
+
+        app = QApplication(sys.argv)
+        viewer = FreakingQtImageViewer(capture)
+        sys.exit(app.exec_())
+
