1f3e56b79f933a1d450074d1c4485e34c97f2806
philipptrenz <mail@philipptrenz.de>
2016-11-12 16:23:53 +0100

    Update image every 0.5s till button gets pressed again

--- pyqt.py
+++ pyqt.py
@@ -1,6 +1,7 @@
 ﻿#! /usr/bin/python3 
 
 import sys
+import time
 from PyQt5.QtWidgets import (QWidget, QHBoxLayout, 
     QLabel, QApplication, QPushButton)
 from PyQt5.QtGui import QPixmap
@@ -12,15 +13,22 @@ class FreakingQtImageViewer(QWidget):
     def __init__(self, function):
         super().__init__()
         self.function = function
-        
         self.initUI(function)
-        
+        self.refresh = False
+    
+      
     def refresh(self):
-        self.function()
-        
-        pixmap = QPixmap("tmp.png")
-        pixmap = pixmap.scaledToWidth(800)
-        self.lbl.setPixmap(pixmap)
+        if !self.refresh:
+            self.refresh = True
+            while self.refresh:
+                self.function()
+                
+                pixmap = QPixmap("tmp.png")
+                pixmap = pixmap.scaledToWidth(800)
+                self.lbl.setPixmap(pixmap)
+                time.sleep(0.5)
+        else: 
+            self.refresh = False
         
     def initUI(self, function):      
 
