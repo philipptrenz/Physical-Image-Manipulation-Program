7669609b5b1d87241d6ec30652daaf8a28b87f25
TheRDavid <david.rosenbusch@googlemail.com>
2016-11-15 17:43:45 +0100

    update

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ from skimage.filters import roberts, sobel, scharr, prewitt
 global WIDTH, HEIGHT
 
 def rgb2gray(rgb_img):
- 	gray_img = array()
+	gray_img = array()
 	numpy.dot(rgb_img[...,:3],[0.2989,0.5870,0.1140], gray_img)
 	return gray_img
 
@@ -30,14 +30,14 @@ def capture():
 	with picamera.array.PiRGBArray(camera) as stream:
 		camera.capture(stream, format='rgb')
 		img = stream.array
-		im = Image.fromarray(img)#.convert('LA')
-		gray_img = rgb2gray(img)
-		im.save('./tmp.png')
+	im = Image.fromarray(img)#.convert('LA')
+	gray_img = rgb2gray(img)
+	im.save('./tmp.png')
 
-		result = hough_ellipse(gray_img, min_size=15, max_size=90)
-		print('detected')
-		result.tolist()
-		print(result)
+	result = hough_ellipse(gray_img, min_size=15, max_size=90)
+	print('detected')
+	result.tolist()
+	print(result)
 
 if __name__ == '__main__':
 	camera = picamera.PiCamera()
