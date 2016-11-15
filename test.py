00836f30b27611475f83e8d2d03d730966ad8d3f
TheRDavid <david.rosenbusch@googlemail.com>
2016-11-15 18:11:13 +0100

    hough ellipses

--- test.py
+++ test.py
@@ -16,22 +16,55 @@ from PIL import Image
 from PyQt5.QtWidgets import QApplication
 from pyqt import FreakingQtImageViewer
 
+from skimage import data, color
+from skimage.feature import canny
 from skimage.transform import hough_ellipse
-from skimage.filters import roberts, sobel, scharr, prewitt
+from skimage.draw import ellipse_perimeter
+
 
 global WIDTH, HEIGHT
 
 def rgb2gray(rgb_img):
 	return numpy.dot(rgb_img[...,:3],[0.2989,0.5870,0.1140])
 
+
+def ellipseDetection(rgb_img):
+	# copy picture, convert to grayscale and detect edges
+	image_rgb = numpy.array(rgb_img, copy=True)
+	image_gray = color.rgb2gray(image_rgb)
+	edges = canny(image_gray, sigma=2.0, low_threshold=0.55, high_threshold=0.8)
+	# Perform a Hough Transform
+	# The accuracy corresponds to the bin size of a major axis.
+	# The value is chosen in order to get a single high accumulator.
+	# The threshold eliminates low accumulators
+	result = hough_ellipse(edges, accuracy=20, threshold=250,
+	                       min_size=100, max_size=120)
+	result.sort(order='accumulator')
+	print(result)
+
+	# Estimated parameters for the ellipse
+	best = list(result[-1])
+	yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+	orientation = best[5]
+	"""
+	# Draw the ellipse on the original image
+	cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+	image_rgb[cy, cx] = (0, 0, 255)
+	# Draw the edge (white) and the resulting ellipse (red)
+	edges = color.gray2rgb(edges)
+	edges[cy, cx] = (250, 0, 0)"""
+
 def capture():
 	with picamera.array.PiRGBArray(camera) as stream:
 		camera.capture(stream, format='rgb')
 		img = stream.array
-	im = Image.fromarray(img)#.convert('LA')
-	original_img = numpy.array(img, copy=True)
-	gray_img = rgb2gray(img)
-	im.save('./tmp.png')
+		im = Image.fromarray(img)#.convert('LA')
+
+		im.save('./tmp.png')
+
+		#original_img = numpy.array(img, copy=True)
+		#gray_img = rgb2gray(img)
+		ellipseDetection(img)
 
 	result = hough_ellipse(gray_img, min_size=15, max_size=90)
 	print('detected')
