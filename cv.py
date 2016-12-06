778fcf5ffa357e482827cb61e8e25c66192885df
TheRDavid <david.rosenbusch@googlemail.com>
2016-12-06 13:45:29 +0100

    blub

--- cv.py
+++ cv.py
@@ -86,7 +86,8 @@ def circle_detection(rgb_img, radMin, radMax, edgesAlready=False):
 		print('radius: ' + str(radius))
 		cx, cy = circle_perimeter(center_y, center_x, radius)
 		circles_rgb = numpy.copy(image_rgb)
-		if cy > 0 and cy < len(circles_rgb) and cx > 0 and cx < len(circles_rgb[0]):
+		shape = circles_rgb.shape
+		if cy > 0 and cy < shape[0] and cx > 0 and cx < shape[1]:
 			if edgesAlready:
 				circles_rgb[cy, cx] = 50
 			else: 
