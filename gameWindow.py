7af1612a653d94780b56c09e6e2332964c37cec6
TheRDavid <david.rosenbusch@googlemail.com>
2016-12-06 15:38:00 +0100

    blub

--- gameWindow.py
+++ gameWindow.py
@@ -145,10 +145,10 @@ class DraughtsGameWindow(QWidget):
 		
 		self.lbl.setPixmap(self.pixmap)
 		if len(self.blackC) == 0 or len(self.blueC) == 0 or len(self.greenC) == 0 or len(self.redC) == 0 :
-			return -1
-		print(final_blue)
-		finalCoords = numpy.array(final_blue, final_green, final_red, final_black)
-		return finalCoords
+			#return -1
+		return -1
+		#finalCoords = numpy.array(final_blue, final_green, final_red, final_black)
+		#return finalCoords
 		
 			
 
