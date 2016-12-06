047106e367ccdd00472fb35abe10f96fbbf1b69a
TheRDavid <david.rosenbusch@googlemail.com>
2016-12-06 15:02:29 +0100

    blub

--- gameWindow.py
+++ gameWindow.py
@@ -177,7 +177,8 @@ class DraughtsGameWindow(QWidget):
 				transformer.estimate(src, dst)
 				transformed_image = tf.warp(input_img, transformer, output_shape=(800, 800)).astype(int)
 				Image.fromarray(transformed_image).save('./transformed.png')
-				
+				print(transformed_image)
+				print(type(transformed_image))
 		camera.close()
 		print(self.corners)
 		
