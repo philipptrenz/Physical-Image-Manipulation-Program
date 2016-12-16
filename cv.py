eb2f16aadcf82aacb68e67829b59832df2544f2c
TheRDavid <david.rosenbusch@googlemail.com>
2016-12-16 19:00:35 +0100

    threading

--- cv.py
+++ cv.py
@@ -83,32 +83,34 @@ def circle_detection(rgb_img, radMin, radMax, edgesAlready=False):
 	accepted_centers = []
 	for idx in numpy.argsort(accums)[::-1][:]: # nach quali sortieren (beste x)
 		center_x, center_y = centers[idx]
-		if(not(center_x >= 1275 or center_y >= 1019 or center_x < 5 or center_y < 5)):
-			count2 = count2 + 1
-			radius = radii[idx]
-			accepted_centers.append(centers[idx])
-			print('radius: ' + str(radius))
-			cx, cy = circle_perimeter(center_y, center_x, radius)
-			circles_rgb = numpy.copy(image_rgb)
-			shape = circles_rgb.shape
-			
-			'''cx = [x for x in cx if x > 0]
-			cx = [x for x in cx if x < 1280]
-			cy = [y for y in cy if y > 0]
-			cy = [y for y in cy if y < 1024]'''
-			
-			circles_rgb[center_y, center_x] = (255, 255, 0)
-			circles_rgb[center_y, center_x+1] = (255, 255, 0)
-			circles_rgb[center_y, center_x-1] = (255, 255, 0)
-			circles_rgb[center_y+1, center_x] = (255, 255, 0)
-			circles_rgb[center_y+1, center_x+1] = (255, 255, 0)
-			circles_rgb[center_y+1, center_x-1] = (255, 255, 0)
-			circles_rgb[center_y-1, center_x] = (255, 255, 0)
-			circles_rgb[center_y-1, center_x+1] = (255, 255, 0)
-			circles_rgb[center_y-1, center_x-1] = (255, 255, 0)
-			'''if edgesAlready:
-				circles_rgb[cy, cx] = 50
-			else: 
-				circles_rgb[cy, cx] = (220, 20, 20)'''
+		if(not(center_x > 480 && center_x < 800))
+			if(not(center_y > 384 && center_y < 640))
+				if(not(center_x >= 1275 or center_y >= 1019 or center_x < 5 or center_y < 5)):
+					count2 = count2 + 1
+					radius = radii[idx]
+					accepted_centers.append(centers[idx])
+					print('radius: ' + str(radius))
+					cx, cy = circle_perimeter(center_y, center_x, radius)
+					circles_rgb = numpy.copy(image_rgb)
+					shape = circles_rgb.shape
+					
+					'''cx = [x for x in cx if x > 0]
+					cx = [x for x in cx if x < 1280]
+					cy = [y for y in cy if y > 0]
+					cy = [y for y in cy if y < 1024]'''
+					
+					circles_rgb[center_y, center_x] = (255, 255, 0)
+					circles_rgb[center_y, center_x+1] = (255, 255, 0)
+					circles_rgb[center_y, center_x-1] = (255, 255, 0)
+					circles_rgb[center_y+1, center_x] = (255, 255, 0)
+					circles_rgb[center_y+1, center_x+1] = (255, 255, 0)
+					circles_rgb[center_y+1, center_x-1] = (255, 255, 0)
+					circles_rgb[center_y-1, center_x] = (255, 255, 0)
+					circles_rgb[center_y-1, center_x+1] = (255, 255, 0)
+					circles_rgb[center_y-1, center_x-1] = (255, 255, 0)
+					'''if edgesAlready:
+						circles_rgb[cy, cx] = 50
+					else: 
+						circles_rgb[cy, cx] = (220, 20, 20)'''
 	print('done -> ', count2)
 	return (image_rgb, accepted_centers, circles_rgb)
