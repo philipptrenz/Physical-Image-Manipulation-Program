4375ee0df620267c4211c1289777f0f8b09b1eb7
philipptrenz <mail@philipptrenz.de>
2017-01-07 16:10:36 +0100

    new stuff

--- cv.py
+++ cv.py
@@ -98,11 +98,11 @@ def circle_detection(rgb_img, radMin, radMax, edgesAlready=False):
 					#print('radius: ' + str(radius))
 
 					# debug -->
-					# paramters: y, x, radius; returns x, y
-					cx, cy = circle_perimeter(center_x+offset, center_y+offset, 23, method='bresenham', shape=None)
-					pixel_color = image_rgb[center_y, center_x]
-					debug_img[cx, cy] = pixel_color
-					print('  accepted circle drawn', center_x, center_y)
+					# paramters: y, x, radius; returns y, x
+					cy, cx = circle_perimeter(center_x+offset, center_y+offset, 23, method='bresenham', shape=None)
+					pixel_color = image_rgb[center_x, center_y]
+					debug_img[cy, cx] = pixel_color
+					#print('  accepted circle drawn', center_x, center_y)
 					# <-- debug end
 
 		# debug -->
