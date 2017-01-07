bbb411ceff73731b37696a3d724baa4091ecb243
philipptrenz <mail@philipptrenz.de>
2017-01-07 17:29:12 +0100

    new stuff

--- cv.py
+++ cv.py
@@ -115,7 +115,10 @@ def circle_detection(rgb_img, radMin, radMax, edgesAlready=False):
 
 					# HSV color test
 
-					print('hsv color: ',color.rgb2hsv([0,0,pixel_color]))
+					x = np.zeros((1,1,3)) # Make a 10 by 20 by 30 array
+					x[0,0] = pixel_color
+					hsv = color.rgb2hsv(x)
+					print('hsv color: ',x[0,0])
 
 					# end
 
